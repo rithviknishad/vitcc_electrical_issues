fae3fc4f6d6d7231ea5864c419475ec7b11a0a51
Aswin Murali <aswinmurali.co@gmail.com>
2021-09-05 12:26:11 +0530

    updates dashboard placeholder

--- lib/routes/dashboard.dart
+++ lib/routes/dashboard.dart
@@ -5,6 +5,11 @@ class DashboardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(
+        child: Center(),
+      ),
+    );
   }
 }
