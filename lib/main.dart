48ccefbd8133a2751d9bd78409eae44dad745bfb
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-19 11:07:19 +0530

    updates main with firebase and route flow control

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:vitcc_electrical_issues/models/user.dart';
+import 'package:vitcc_electrical_issues/routes/authenticate.dart';
 import 'package:vitcc_electrical_issues/routes/dashboard.dart';
 import 'package:vitcc_electrical_issues/services/auth.dart';
+import 'package:vitcc_electrical_issues/shared/loading_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
-
   runApp(
     MaterialApp(
-      title: 'VITCC Electrical Issues',
+      debugShowCheckedModeBanner: false,
+      title: 'VITCC Electrical Issue Tracker',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DashboardPage(),
+      home: FutureBuilder<FirebaseApp>(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Loading screen, when initializing firebase app.
+          if (!snapshot.hasData) {
+            return Loading();
+          }
+
+          // Loading screen, when attempting to authenticate w/ firebase.
+          return StreamBuilder<UserSnapshot?>(
+              stream: AuthService.user,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Loading();
+                }
+
+                // Authenitcated
+                if (snapshot.data is UserSnapshot) {
+                  return DashboardPage();
+                }
+
+                return Authenticate();
+              });
+        },
+      ),
     ),
   );
 }
@@ -30,7 +56,7 @@ class AppHome extends StatelessWidget {
           children: [
             SignInButton(
               Buttons.Google,
-              onPressed: () => AuthService().signInWithGoogle(),
+              onPressed: () => AuthService.signInWithGoogle(),
             ),
           ],
         ),
