a1f1fcd6e57f42a6e1caa4658738497495d15019
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-19 12:09:31 +0530

    adds theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:vitcc_electrical_issues/models/user.dart';
 import 'package:vitcc_electrical_issues/routes/authenticate.dart';
 import 'package:vitcc_electrical_issues/routes/dashboard.dart';
@@ -10,13 +9,18 @@ import 'package:vitcc_electrical_issues/shared/loading_widget.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(
-    MaterialApp(
+  runApp(ElectricalIssueTrackerApp());
+}
+
+class ElectricalIssueTrackerApp extends StatelessWidget {
+  const ElectricalIssueTrackerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'VITCC Electrical Issue Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: _theme,
       home: FutureBuilder<FirebaseApp>(
         future: Firebase.initializeApp(),
         builder: (context, snapshot) {
@@ -42,25 +46,75 @@ void main() async {
               });
         },
       ),
-    ),
-  );
-}
-
-class AppHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SignInButton(
-              Buttons.Google,
-              onPressed: () => AuthService.signInWithGoogle(),
-            ),
-          ],
-        ),
-      ),
     );
   }
+
+  static const _primary = Color.fromARGB(255, 0, 38, 70);
+  static const _accent = Color.fromARGB(255, 245, 248, 253);
+  static const _secondary = Color(0xFFFFCA28);
+
+  static final _theme = ThemeData(
+    fontFamily: 'Ubuntu',
+    brightness: Brightness.light,
+    primaryColor: _primary,
+    accentColor: _accent,
+    appBarTheme: AppBarTheme(
+      brightness: Brightness.light,
+      color: Colors.white,
+      elevation: 0,
+      actionsIconTheme: const IconThemeData(color: _primary),
+    ),
+    iconTheme: const IconThemeData(color: _primary),
+
+    scaffoldBackgroundColor: _accent,
+    snackBarTheme: const SnackBarThemeData(
+      backgroundColor: _primary,
+      actionTextColor: _secondary,
+      disabledActionTextColor: Colors.grey,
+      contentTextStyle: TextStyle(color: Colors.white),
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(_primary),
+        textStyle: MaterialStateProperty.all(TextStyle(
+          fontFamily: 'Ubuntu',
+          color: _accent,
+          fontWeight: FontWeight.w500,
+        )),
+      ),
+    ),
+
+    // Bottom Navigation Bar Theme
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: _primary,
+      selectedItemColor: _secondary,
+      unselectedItemColor: Colors.blueGrey,
+      type: BottomNavigationBarType.fixed,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: _accent,
+      enabledBorder: UnderlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide.none,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: _primary),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Colors.red),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide(color: Colors.red),
+      ),
+      labelStyle: const TextStyle(color: _primary, fontSize: 14),
+      focusColor: _primary,
+    ),
+  );
 }
