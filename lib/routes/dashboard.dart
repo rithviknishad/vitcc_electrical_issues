b8727ed2116682a05feb96aa7a60871a2c7004e7
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-19 22:24:52 +0530

    partially edits dashboard

--- lib/routes/dashboard.dart
+++ lib/routes/dashboard.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animator/flutter_animator.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _selectedPage = 2;
+
+  int get selectedPage => _selectedPage;
+
+  set selectedPage(int value) {
+    if (value != selectedPage) {
+      setState(() => _selectedPage = value);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
     return Scaffold(
+      backgroundColor: theme.accentColor,
       appBar: AppBar(
-        title: Text('VIT'),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Text("Hi"),
-            ],
+        title: FadeInLeft(
+          preferences: const AnimationPreferences(
+            duration: Duration(
+              milliseconds: 400,
+            ),
           ),
+          child: Text(
+            'Bookings',
+            style: TextStyle(
+                fontWeight: FontWeight.w400, color: theme.primaryColor),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [],
         ),
       ),
     );
