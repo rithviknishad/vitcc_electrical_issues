c89abd9c43e48b4437bca86edfe1b91247d2e2a0
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-21 15:13:56 +0530

    adds watch method for issue

--- lib/models/issue.dart
+++ lib/models/issue.dart
@@ -82,6 +82,27 @@ class Issue with _$Issue {
 
   static final activeIssues = _activeRef.snapshots();
 
+  /// Watch the specified issue reference.
+  ///
+  /// Throws exception if [ref] does not belong to active or resolved issue
+  /// collection.
+  static Stream<IssueSnapshot> watch(DocumentReference ref) {
+    // Check if ref belongs to active issues.
+    if (ref.parent == _activeRef) {
+      return _activeRef.doc(ref.id).snapshots();
+    }
+
+    // Check if ref belongs to resolved issues.
+    if (ref.parent == _resolvedRef) {
+      return _resolvedRef.doc(ref.id).snapshots();
+    }
+
+    // If the passed ref does not belong to any of the supported collections.
+    throw Exception(
+      "$ref's parent is not $_activeRef or $_resolvedRef. Unable to decide how to watch $ref.",
+    );
+  }
+
   /// Creates a new active issue and returns a snapshot of it.
   ///
   /// This future throws an exception if the user has no permission to create
