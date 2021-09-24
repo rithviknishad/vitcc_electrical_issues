ba641d2b6f94eb4a6233de95222efd94220f3025
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-24 12:02:02 +0530

    minify code

--- lib/routes/authenticate.dart
+++ lib/routes/authenticate.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 import 'package:vitcc_electrical_issues/services/auth_service.dart';
 import 'package:vitcc_electrical_issues/shared/loading_widget.dart';
+import 'package:vitcc_electrical_issues/shared/text_field_widget.dart';
 import 'package:vitcc_electrical_issues/shared/wave_widget.dart';
 
-class Authenticate extends StatelessWidget {
+class AuthenticatePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.light,
-        systemNavigationBarColor: Colors.white30,
-        systemNavigationBarIconBrightness: Brightness.dark,
-      ),
-      child: AuthenticateView(),
-    );
-  }
+  _AuthenticatePageState createState() => _AuthenticatePageState();
 }
 
-class AuthenticateView extends StatefulWidget {
-  @override
-  _AuthenticateViewState createState() => _AuthenticateViewState();
-}
-
-class _AuthenticateViewState extends State<AuthenticateView>
+class _AuthenticatePageState extends State<AuthenticatePage>
     with WidgetsBindingObserver {
+  /// To validate the form that may contain vit email for sign in with link.
   final formKey = GlobalKey<FormState>();
+
+  /// For sign in with link using VIT mail.
   final vitMailController = TextEditingController();
+
+  /// Whether the specified vit mail is valid.
+  bool vitMailIsValid = false;
+
+  /// Whether a process related to authentication / auth_service is running.
   bool isLoading = false;
 
   @override
@@ -54,66 +50,95 @@ class _AuthenticateViewState extends State<AuthenticateView>
     final size = MediaQuery.of(context).size;
     final keyboardIsOpen = MediaQuery.of(context).viewInsets.bottom > 0;
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: size.height - 200,
-            color: theme.primaryColor,
-          ),
-          AnimatedPositioned(
-            duration: Duration(seconds: 1),
-            curve: Curves.fastLinearToSlowEaseIn,
-            top: keyboardIsOpen ? -size.height / 3.7 : 0.0,
-            child: WaveWidget(
-              size: size,
-              yOffset: size.height / 3.0,
-              color: Colors.white,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.white30,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              height: size.height - 200,
+              color: theme.primaryColor,
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: size.height * 0.15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: size.width * 0.8,
-                  child: Text(
-                    'Electrical Issue Tracker',
-                    style: TextStyle(
-                      color: keyboardIsOpen
-                          ? theme.primaryColor
-                          : theme.colorScheme.secondary,
-                      fontSize: 36.0,
-                      fontWeight: FontWeight.w300,
+            AnimatedPositioned(
+              duration: Duration(seconds: 1),
+              curve: Curves.fastLinearToSlowEaseIn,
+              top: keyboardIsOpen ? -size.height / 3.7 : 0.0,
+              child: WaveWidget(
+                size: size,
+                yOffset: size.height / 3.0,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: size.height * 0.15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: size.width * 0.8,
+                    child: Text(
+                      'Electrical Issue Tracker',
+                      style: TextStyle(
+                        color: keyboardIsOpen
+                            ? theme.primaryColor
+                            : theme.colorScheme.secondary,
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.w300,
+                      ),
+                      textAlign: TextAlign.center,
+                      softWrap: true,
                     ),
-                    textAlign: TextAlign.center,
-                    softWrap: true,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                _Button(
-                  text: 'Sign in with Google',
-                  hasBorder: false,
-                  onTap: () async {
-                    setState(() => isLoading = true);
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Form(
+                    key: formKey,
+                    child: TextFieldWidget(
+                      controller: vitMailController,
+                      hintText: 'VIT Email',
+                      obscureText: false,
+                      prefixIconData: FontAwesome5.id_badge,
+                      suffixIconData: vitMailIsValid ? Icons.check : null,
+                      onChanged: (_) {
+                        final result =
+                            formKey.currentState?.validate() ?? false;
 
-                    await AuthService.signInWithGoogle();
+                        if (vitMailIsValid != result) {
+                          setState(() => vitMailIsValid = result);
+                        }
+                      },
+                      validator:
+                          EmailValidator(errorText: 'Invalid Email Address'),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  _Button(
+                    text: 'Sign in with Google',
+                    hasBorder: false,
+                    onTap: () async {
+                      setState(() => isLoading = true);
 
-                    setState(() => isLoading = false);
-                  },
-                )
-              ],
+                      await AuthService.signInWithGoogle();
+
+                      setState(() => isLoading = false);
+                    },
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
