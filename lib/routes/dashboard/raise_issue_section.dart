150f4eca6204ce6b05c755ec25e5207035a54aba
Rithvik Nishad <rithvikn2001@gmail.com>
2021-09-30 22:45:33 +0530

    adds raise isue section

---
+++ lib/routes/dashboard/raise_issue_section.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:vitcc_electrical_issues/routes/dashboard/dashboard.dart';
+
+class RaiseAnIssueSection extends StatelessWidget {
+  const RaiseAnIssueSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: theme.hoverColor,
+        borderRadius: BorderRadius.circular(20),
+        // color: theme.colorScheme.surface,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Having an electrical issue in campus?',
+            style: TextStyle(
+              color: theme.primaryColor,
+            ),
+          ),
+          SizedBox(height: 18),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton.extended(
+                onPressed: () =>
+                    DashboardPage.of(context).showRaiseNewIssueForm(context),
+                tooltip: 'Click to raise a new issue.',
+                label: Text('Raise a new issue'),
+                icon: Icon(FontAwesome5.feather),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
