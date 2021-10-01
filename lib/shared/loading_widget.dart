81f06157468c7ef811b57eb3054659b82151c4a1
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-01 21:57:21 +0530

    support for alt in loading widget

--- lib/shared/loading_widget.dart
+++ lib/shared/loading_widget.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
+  final bool alternate;
+
+  const Loading([this.alternate = false]);
+
+  factory Loading.alt() => Loading(true);
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
+    final bgColor =
+        alternate ? theme.colorScheme.secondary : theme.primaryColor;
+
+    final fgColor =
+        alternate ? theme.primaryColor : theme.colorScheme.secondary;
+
     return Material(
-      color: theme.primaryColor,
+      color: bgColor,
       child: Center(
         child: SpinKitPulse(
-          color: theme.colorScheme.secondary,
+          color: fgColor,
           size: 50.0,
         ),
       ),
