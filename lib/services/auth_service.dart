ed0121ad92b99dc7568558e25f736b21641066d1
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-01 23:27:31 +0530

    clean

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:vitcc_electrical_issues/models/user.dart';
 
 class AuthService {
   static final firebaseAuth = FirebaseAuth.instance;
