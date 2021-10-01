ff8f1fbffc162677479a91e32acdff8530cbf522
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-01 23:27:23 +0530

    clean

--- lib/routes/dashboard/active_issues.dart
+++ lib/routes/dashboard/active_issues.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:provider/provider.dart';
