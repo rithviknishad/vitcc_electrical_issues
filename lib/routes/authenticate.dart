6ecbb8a90c0aff0bc69459fde6cac66a19632aa4
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-02 10:27:52 +0530

    adds authenticated widget for type safe state mgmt

--- lib/routes/authenticate.dart
+++ lib/routes/authenticate.dart
@@ -1,15 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:vitcc_electrical_issues/models/misc.dart';
+import 'package:vitcc_electrical_issues/models/user.dart';
 import 'package:vitcc_electrical_issues/services/auth_service.dart';
 import 'package:vitcc_electrical_issues/shared/loading_widget.dart';
 import 'package:vitcc_electrical_issues/shared/wave_widget.dart';
 
-class AuthenticatePage extends StatefulWidget {
+class Authenticated extends StatelessWidget {
+  final Widget child;
+
+  const Authenticated({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: AuthService.user,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Loading();
+        }
+
+        final currentUser = snapshot.data;
+
+        if (currentUser is User) {
+          final userStream = PlatformUser.watch(currentUser);
+          return MultiProvider(
+            providers: [
+              StreamProvider.value(value: Misc.watch, initialData: null),
+              StreamProvider.value(value: userStream, initialData: null),
+            ],
+            builder: (context, _) {
+              final userSnapshot = Provider.of<UserSnapshot?>(context);
+              final miscSnapshot = Provider.of<MiscSnapshot?>(context);
+
+              if (userSnapshot == null || miscSnapshot == null) {
+                return Loading();
+              }
+
+              return MultiProvider(
+                providers: [
+                  Provider.value(value: userSnapshot),
+                  Provider.value(value: miscSnapshot),
+                ],
+                child: child,
+              );
+            },
+          );
+        }
+
+        return _AuthenticatePage();
+      },
+    );
+  }
+}
+
+class _AuthenticatePage extends StatefulWidget {
   @override
   _AuthenticatePageState createState() => _AuthenticatePageState();
 }
 
-class _AuthenticatePageState extends State<AuthenticatePage> {
+class _AuthenticatePageState extends State<_AuthenticatePage> {
   /// Whether a process related to authentication / auth_service is running.
   bool isLoading = false;
 
