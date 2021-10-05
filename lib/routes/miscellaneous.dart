8449771f79d6a807aead06c6e4e16b50b8d517fe
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-05 22:39:50 +0530

    miscellaneous almost completed

--- lib/routes/miscellaneous.dart
+++ lib/routes/miscellaneous.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/link.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:vitcc_electrical_issues/main.dart';
 import 'package:vitcc_electrical_issues/models/maintainer.dart';
 import 'package:vitcc_electrical_issues/models/user.dart';
@@ -29,13 +28,8 @@ class MiscellaneousDialog extends StatelessWidget {
     final theme = Theme.of(context);
     final materialLocalizations = MaterialLocalizations.of(context);
 
-    final user = userSnapshot.user;
-
-    final attributeValueStyle = theme.textTheme.caption?.copyWith(
-      fontWeight: FontWeight.bold,
-    );
-
     return AlertDialog(
+      backgroundColor: theme.scaffoldBackgroundColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
       ),
@@ -80,24 +74,11 @@ class MiscellaneousDialog extends StatelessWidget {
             ],
           ),
           SizedBox(height: 20),
-          buildUserAttribute(
-            'Signed in as',
-            user.name ?? user.email ?? userSnapshot.id,
-            theme.textTheme.caption,
-            attributeValueStyle,
-          ),
-          buildUserAttribute(
-            'Email',
-            '${user.email}',
-            theme.textTheme.caption,
-            attributeValueStyle,
-          ),
-          buildUserAttribute(
-            'UID',
-            userSnapshot.id,
-            theme.textTheme.caption,
-            attributeValueStyle,
-          ),
+
+          // Current user attributes
+          ...buildUserAttributes(theme, userSnapshot),
+
+          // Sign out button
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextButton.icon(
@@ -106,44 +87,18 @@ class MiscellaneousDialog extends StatelessWidget {
               label: Text('Sign out'),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Project Maintained by',
-              style: theme.textTheme.bodyText2?.copyWith(
-                color: theme.primaryColor,
-              ),
-            ),
-          ),
-          for (final maintainer in Maintainer.all)
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 26,
-                  width: 26,
-                  child: CircleAvatar(
-                    foregroundImage:
-                        NetworkImage('${maintainer.photoURL}&s=70'),
-                    child: Text(maintainer.username[0]),
-                  ),
-                ),
-                SizedBox(width: 8),
-                Link(
-                  uri: Uri.tryParse(maintainer.uri),
-                  builder: (context, followLink) {
-                    return TextButton(
-                      onPressed: followLink,
-                      child: Text(
-                        'github.com/${maintainer.username}',
-                        style: theme.textTheme.caption
-                            ?.copyWith(letterSpacing: 0.5),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            )
+
+          // Project maintained by
+          SizedBox(height: 8),
+          ...buildProjectMaintainers(theme),
+
+          // Facing issues using the platform
+          SizedBox(height: 16),
+          ...buildIssueTrackerWidgets(theme),
+
+          // Contribution pointers
+          SizedBox(height: 16),
+          ...buildProjectRepositoryAttributes(theme),
         ],
       ),
       actions: [
@@ -168,6 +123,80 @@ class MiscellaneousDialog extends StatelessWidget {
     );
   }
 
+  List<Widget> buildUserAttributes(ThemeData theme, UserSnapshot userSnapshot) {
+    final attributeValueStyle = theme.textTheme.caption?.copyWith(
+      fontWeight: FontWeight.bold,
+    );
+
+    final user = userSnapshot.user;
+
+    final permissions = {
+      'Create new issue': user.scope.canCreateIssue,
+      'View issues raised by you': true,
+      'Purge active issues raised by you': true,
+      'Purge resolved issues raised by you': false,
+      'View all active issues raised by anyone': user.scope.canViewActiveIssues,
+      'View all resolved issues raised by anyone':
+          user.scope.canViewResolvedIssues,
+      'Resolve an active issue': user.scope.canResolveIssue,
+      'Purge active issues NOT raised by you': user.scope.canPurgeIssue,
+      'Purge resolved issues NOT raised by you': false,
+    };
+
+    return [
+      buildUserAttribute(
+        'Signed in as',
+        user.name ?? user.email ?? userSnapshot.id,
+        theme.textTheme.caption,
+        attributeValueStyle,
+      ),
+      buildUserAttribute(
+        'Email',
+        '${user.email}',
+        theme.textTheme.caption,
+        attributeValueStyle,
+      ),
+      buildUserAttribute(
+        'UID',
+        userSnapshot.id,
+        theme.textTheme.caption,
+        attributeValueStyle,
+      ),
+      SizedBox(height: 8),
+      Padding(
+        padding: const EdgeInsets.all(4),
+        child: Text(
+          'You have the following permissions ',
+          style: theme.textTheme.caption,
+        ),
+      ),
+      SizedBox(height: 6),
+      for (final permission in permissions.entries)
+        Padding(
+          padding: const EdgeInsets.all(4),
+          child: Row(
+            children: [
+              SizedBox(width: 8),
+              Icon(
+                permission.value ? FontAwesome5.check : FontAwesome5.times,
+                color: permission.value ? Colors.green : Colors.red,
+                size: 12,
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  permission.key,
+                  style: theme.textTheme.caption?.copyWith(
+                    color: permission.value ? Colors.green : Colors.red,
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+    ];
+  }
+
   Widget buildUserAttribute(
     String key,
     String value,
@@ -184,5 +213,143 @@ class MiscellaneousDialog extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  List<Widget> buildIssueTrackerWidgets(ThemeData theme) {
+    return [
+      Text(
+        'Facing issues using the platform?',
+        style: theme.textTheme.bodyText2?.copyWith(
+          color: theme.primaryColor,
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Link(
+              uri: Uri.tryParse(ElectricalIssueTrackerApp.appIssueTracker),
+              builder: (context, followLink) {
+                return MaterialButton(
+                  padding: const EdgeInsets.all(1),
+                  onPressed: followLink,
+                  child: Text(
+                    'View/report on GitHub',
+                    style: theme.textTheme.caption,
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Text(
+                'or',
+                style: theme.textTheme.caption?.copyWith(
+                  color: theme.primaryColor,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Link(
+              uri: Uri.tryParse(
+                'mailto:${ElectricalIssueTrackerApp.appIssueTrackerMailing}',
+              ),
+              builder: (context, followLink) {
+                return MaterialButton(
+                  padding: const EdgeInsets.all(1),
+                  onPressed: followLink,
+                  child: Text(
+                    'Send us a mail',
+                    style: theme.textTheme.caption,
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    ];
+  }
+
+  List<Widget> buildProjectMaintainers(ThemeData theme) {
+    return [
+      Text(
+        'Project maintained by',
+        style: theme.textTheme.bodyText2?.copyWith(
+          color: theme.primaryColor,
+        ),
+      ),
+      SizedBox(height: 8),
+      for (final maintainer in Maintainer.all)
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 26,
+              width: 26,
+              child: CircleAvatar(
+                foregroundImage: NetworkImage('${maintainer.photoURL}&s=70'),
+                child: Text(maintainer.username[0]),
+              ),
+            ),
+            SizedBox(width: 8),
+            Link(
+              uri: Uri.tryParse(maintainer.uri),
+              builder: (context, followLink) {
+                return TextButton(
+                  onPressed: followLink,
+                  child: Text(
+                    'github.com/${maintainer.username}',
+                    style:
+                        theme.textTheme.caption?.copyWith(letterSpacing: 0.5),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+    ];
+  }
+
+  List<Widget> buildProjectRepositoryAttributes(ThemeData theme) {
+    return [
+      Text(
+        'Willing to contribute to this open source project?',
+        style: theme.textTheme.bodyText2?.copyWith(
+          color: theme.primaryColor,
+        ),
+      ),
+      Link(
+        uri: Uri.tryParse(ElectricalIssueTrackerApp.appRepository),
+        builder: (context, followLink) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextButton.icon(
+              style: TextButton.styleFrom(
+                backgroundColor: theme.primaryColor,
+              ),
+              onPressed: followLink,
+              icon: Icon(
+                FontAwesome5.github,
+                color: Colors.white,
+              ),
+              label: Text(
+                'vitcc_electrical_issues',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    ];
   }
 }
