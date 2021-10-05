582f59eeab8014b367bf513e6c5b639c0c7cf4a5
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-05 22:39:17 +0530

    material you theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,14 @@ class ElectricalIssueTrackerApp extends StatelessWidget {
   static const appName = 'Electrical Issue Tracker';
   static const Widget? appIcon = null; // TODO: add application logo here also
   static const appLegalese = 'GNU General Public License v3.0';
+  // Move to Misc
+  static const appIssueTracker =
+      'https://github.com/rithviknishad/vitcc_electrical_issues/issues';
+  // Move to Misc
+  static const appIssueTrackerMailing = 'rithvik.nishad2019@vitstudent.ac.in';
+
+  static const appRepository =
+      'https://github.com/rithviknishad/vitcc_electrical_issues/';
 
   const ElectricalIssueTrackerApp({Key? key}) : super(key: key);
 
@@ -36,17 +44,15 @@ class ElectricalIssueTrackerApp extends StatelessWidget {
     );
   }
 
-  static const _primary = Color.fromARGB(255, 0, 38, 70);
-  static const _accent = Color.fromARGB(255, 245, 248, 253);
-  static const _secondary = Color(0xFFFFCA28);
+  static const _primary = Color(0xFF924642);
+  static const _accent = Color(0xFFFCEDEA);
+  static const _background = Color(0xFFFEF9F7);
   static const _fontFamily = 'Ubuntu';
 
   static get _theme => ThemeData(
         fontFamily: _fontFamily,
         brightness: Brightness.light,
-
         primaryColor: _primary,
-
         colorScheme: ColorScheme.light(
           primary: _primary,
           onPrimary: _accent,
@@ -56,16 +62,15 @@ class ElectricalIssueTrackerApp extends StatelessWidget {
           background: _accent,
           onBackground: _primary,
         ),
-
-        scaffoldBackgroundColor: _accent,
-
+        scaffoldBackgroundColor: _background,
+        cardColor: _accent,
         appBarTheme: AppBarTheme(
-          color: Colors.white,
+          color: Color(0xFFFEF9F7),
           iconTheme: IconThemeData(
             color: _primary,
           ),
           systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Colors.white,
+            statusBarColor: _background,
             statusBarIconBrightness: Brightness.dark,
           ),
           elevation: 0,
@@ -78,44 +83,16 @@ class ElectricalIssueTrackerApp extends StatelessWidget {
             color: _primary,
           ),
         ),
-
         iconTheme: const IconThemeData(color: _primary),
-
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: _primary,
-          foregroundColor: _accent,
-        ),
-
         snackBarTheme: const SnackBarThemeData(
           backgroundColor: _primary,
-          actionTextColor: _secondary,
+          actionTextColor: Colors.amber,
           disabledActionTextColor: Colors.grey,
           contentTextStyle: TextStyle(
             fontFamily: _fontFamily,
             color: Colors.white,
             letterSpacing: 0.5,
           ),
-        ),
-
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(_primary),
-            textStyle: MaterialStateProperty.all(TextStyle(
-              fontFamily: _fontFamily,
-              color: _accent,
-              fontWeight: FontWeight.w500,
-            )),
-          ),
-        ),
-
-        // Bottom Navigation Bar Theme
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: _primary,
-          selectedItemColor: _secondary,
-          unselectedItemColor: Colors.blueGrey,
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
         ),
         inputDecorationTheme: InputDecorationTheme(
           filled: true,
