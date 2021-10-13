cabfdab04bffa3c5403aea5419df563701932012
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-13 11:16:04 +0530

    clean unused imports

--- lib/shared/text_field_widget.dart
+++ lib/shared/text_field_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TextFieldWidget extends StatelessWidget {
   final String hintText;
