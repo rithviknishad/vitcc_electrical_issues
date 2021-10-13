f1a07ab0b12222268fa0257bec478b2fa20ad5b9
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-13 10:02:24 +0530

    adds resolved issues page

--- lib/routes/resolved_issues.dart
+++ lib/routes/resolved_issues.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vitcc_electrical_issues/models/user.dart';
 
 class ResolvedIssuesPage extends StatelessWidget {
   const ResolvedIssuesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('hell'),
+        ),
+      ),
+    );
   }
 }
