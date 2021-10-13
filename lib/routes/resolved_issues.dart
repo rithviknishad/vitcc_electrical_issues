94127ed8b4a70dd4c452eafa78f810fcf9224d9f
Rithvik Nishad <rithvikn2001@gmail.com>
2021-10-13 11:15:58 +0530

    clean unused imports

--- lib/routes/resolved_issues.dart
+++ lib/routes/resolved_issues.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:vitcc_electrical_issues/models/user.dart';
 
 class ResolvedIssuesPage extends StatelessWidget {
   const ResolvedIssuesPage({Key? key}) : super(key: key);
